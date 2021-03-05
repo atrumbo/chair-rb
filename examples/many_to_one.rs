70c55c74d6ae586662ffe12d481c3e747ecab055
atrumbo <atrumbo@users.noreply.github.com>
2021-03-04 22:02:33 -0600

    Add many_to_one example. Some refactor of one_to_one example

---
+++ examples/many_to_one.rs
@@ -0,0 +1,105 @@
+use rust_agrona_rb::concurrent::ring_buffer::{ManyToOneRingBuffer, RingBuffer};
+use rust_agrona_rb::concurrent::AtomicBuffer;
+use rust_agrona_rb::mem::Align16;
+use std::mem::size_of;
+use std::sync::Arc;
+use std::thread;
+use std::time::Instant;
+use std::sync::atomic::{AtomicI32, Ordering};
+
+
+const PRODUCER_COUNT: u32 = 3;
+const MESSAGES_PER_PRODUCER: u32 = 100_000_000;
+const POISON_MESSAGE_TYPE: i32 = 42;
+
+fn main() {
+    println!("Many to One Ring Buffer Example");
+    let mut buf: Align16<[u8; 1024 + 768]> = Align16::new([0; 1024 + 768]);
+    let buffer = AtomicBuffer::wrap(buf.as_mut_ptr(), buf.len() as u32);
+    // let ring_buffer = OneToOneRingBuffer::new(buffer);
+    let ring_buffer = Arc::new(ManyToOneRingBuffer::new(buffer));
+    let producer_id = Arc::new(AtomicI32::new(1));
+
+    let mut threads = vec![];
+
+    for _ in 0..PRODUCER_COUNT {
+        let producer_id = producer_id.clone();
+        let ring_buffer = ring_buffer.clone();
+        threads.push( thread::spawn(move || {
+            let mut src_buf: [u8; 128] = [0; 128];
+            let src_buffer = AtomicBuffer::wrap(src_buf.as_mut_ptr(), src_buf.len() as u32);
+
+            let producer_id = producer_id.fetch_add(1, Ordering::SeqCst);
+
+            let mut sent = 0;
+
+            while sent < MESSAGES_PER_PRODUCER {
+                src_buffer.put_i64(0, sent as i64);
+
+                while !ring_buffer.write(producer_id, &src_buffer, 0, size_of::<i64>() as i32) {
+                    thread::yield_now();
+                }
+                sent += 1;
+
+                if sent % (MESSAGES_PER_PRODUCER / 10) == 0 {
+                    println!("Producer #{}\t- Written {} message to ring buffer", producer_id, sent);
+                }
+
+
+
+            }
+            src_buffer.put_i64(0, POISON_MESSAGE_TYPE as i64);
+            println!(
+                "Producer #{}\t- Sending poison after publishing {} messages to ring buffer",
+                producer_id, sent
+            );
+
+            while !ring_buffer.write(POISON_MESSAGE_TYPE, &src_buffer, 0, size_of::<i64>() as i32) {
+                thread::yield_now();
+            }
+
+        }));
+    }
+
+    let mut times_called = 0;
+
+    let mut poison_count = 0;
+
+    let instant = Instant::now();
+
+    while poison_count < PRODUCER_COUNT {
+
+        ring_buffer.read(
+            |msg_type_id, src_buffer, src_index, _src_length| {
+                times_called += 1;
+
+                if times_called % (MESSAGES_PER_PRODUCER / 10) == 0 {
+                    println!("Consumer\t- Read {} messages from ring buffer", times_called);
+                }
+
+                let value = src_buffer.get_i64(src_index);
+
+                if msg_type_id == POISON_MESSAGE_TYPE {
+                    poison_count += 1;
+                    println!(
+                        "Consumer\t- Got Poison - msg_type_id: {} value: {}, received total {}",
+                        msg_type_id, value, times_called
+                    );
+                }
+            },
+            u32::max_value(),
+        );
+    }
+
+    let elapsed = instant.elapsed();
+    println!(
+        "Time to receive {} messages {} milliseconds {} message per second",
+        times_called,
+        elapsed.as_millis(),
+        times_called / elapsed.as_secs() as u32
+    );
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+}
--- examples/one_to_one.rs
+++ examples/one_to_one.rs
@@ -6,8 +6,12 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Instant;
 
+
+const MESSAGES_TO_PRODUCE: u32 = 1_000_000_000;
+const POISON_MESSAGE_TYPE: i32 = 42;
+
 fn main() {
-    println!("Hello from an example!");
+    println!("One to One Ring Buffer Example");
     let mut buf: Align16<[u8; 1024 + 768]> = Align16::new([0; 1024 + 768]);
     let buffer = AtomicBuffer::wrap(buf.as_mut_ptr(), buf.len() as u32);
     // let ring_buffer = OneToOneRingBuffer::new(buffer);
@@ -19,33 +23,25 @@ fn main() {
         let src_buffer = AtomicBuffer::wrap(src_buf.as_mut_ptr(), src_buf.len() as u32);
 
         let mut sent = 0;
-        loop {
-            // println!("hi number {} from the spawned thread!", i);
+        while sent < MESSAGES_TO_PRODUCE {
             src_buffer.put_i64(0, sent as i64);
-            loop {
-                if rb.write(1, &src_buffer, 0, size_of::<i64>() as i32) {
-                    sent += 1;
-                    break;
-                }
-            }
-            if sent % 100_000_000 == 0 {
-                println!("Written {} message to ring buffer", sent);
+
+            while !rb.write(1, &src_buffer, 0, size_of::<i64>() as i32) {}
+            sent += 1;
+
+            if sent % (MESSAGES_TO_PRODUCE / 10) == 0 {
+                println!("Producer - Written {} message to ring buffer", sent);
             }
 
-            if sent == 1_000_000_000 {
-                break;
-            }
         }
-        src_buffer.put_i64(0, 42);
+        src_buffer.put_i64(0, POISON_MESSAGE_TYPE as i64);
         println!(
-            "ProducerThread: Sending poison after publishing {} messages to ringbuffer",
+            "Producer - Sending poison after publishing {} messages to ring buffer",
             sent
         );
-        loop {
-            if rb.write(42, &src_buffer, 0, size_of::<i64>() as i32) {
-                break;
-            }
-        }
+
+        while !rb.write(POISON_MESSAGE_TYPE, &src_buffer, 0, size_of::<i64>() as i32) {}
+
     });
 
     let mut times_called = 0;
@@ -54,17 +50,14 @@ fn main() {
 
     let instant = Instant::now();
 
-    loop {
-        if is_poison {
-            break;
-        }
+    while !is_poison {
 
         ring_buffer.read(
             |msg_type_id, src_buffer, src_index, _src_length| {
                 times_called += 1;
 
-                if times_called % 100_000_000 == 0 {
-                    println!("Read {} messages from ringbuffer", times_called);
+                if times_called % (MESSAGES_TO_PRODUCE / 10) == 0 {
+                    println!("Consumer - Read {} messages from ring buffer", times_called);
                 }
 
                 let value = src_buffer.get_i64(src_index);
@@ -72,7 +65,7 @@ fn main() {
                 if msg_type_id == 42 {
                     is_poison = true;
                     println!(
-                        "ConsumerThread: Got Poison - msg_type_id: {} value: {}, received total {}",
+                        "Consumer - Got Poison - msg_type_id: {} value: {}, received total {}",
                         msg_type_id, value, times_called
                     );
                 }
@@ -83,9 +76,10 @@ fn main() {
 
     let elapsed = instant.elapsed();
     println!(
-        "Time to receive {} messages {} milliseconds",
+        "Time to receive {} messages {} milliseconds {} message per second",
         times_called,
-        elapsed.as_millis()
+        elapsed.as_millis(),
+        times_called / elapsed.as_secs() as u32
     );
 
     handle.join().unwrap();
